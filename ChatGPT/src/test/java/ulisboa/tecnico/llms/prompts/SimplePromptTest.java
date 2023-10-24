3e0b14cee501aee9c0ba418693f5837b77e24a39
BlackKnight625 <43484611+BlackKnight625@users.noreply.github.com>
2023-10-24 12:20:02 +0100

    Added new test

--- ChatGPT/src/test/java/ulisboa/tecnico/llms/prompts/SimplePromptTest.java
+++ ChatGPT/src/test/java/ulisboa/tecnico/llms/prompts/SimplePromptTest.java
@@ -11,4 +11,17 @@ public class SimplePromptTest extends CommonTest {
 
         System.out.println("Prompted ChatGPT with the prompt (" + prompt + "). Its response is:\n\n----" + response + "\n----");
     }
+
+    @Test
+    public void memoryTest() {
+        String prompt1 = "When I say 'Hello There', reply with 'General Kenobi'";
+        String response1 = getManager().promptSync(prompt1);
+
+        System.out.println("Prompted ChatGPT with the prompt (" + prompt1 + "). Its response is:\n\n----" + response1 + "\n----");
+
+        String prompt2 = "Hello there!";
+        String response2 = getManager().promptSync(prompt2);
+
+        System.out.println("Prompted ChatGPT with the prompt (" + prompt2 + "). Its response is:\n\n----" + response2 + "\n----");
+    }
 }
